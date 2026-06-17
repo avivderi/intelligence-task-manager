f524a6c4ebdd6d1827aacb98161a26427bd12955
aviv deri <aviv322deri@gmail.com>
2026-06-17 11:39:41 +0300

    add mysql conection

---
+++ database/db_connection.py
@@ -0,0 +1,70 @@
+import mysql.connector
+
+class DB_connection:
+    def __init__(self):
+        self.conn = None
+        self.get_conncation()
+        self.create_database()
+        self.close_db()
+
+    def connect(self):
+        self.conn = mysql.connector.connect(
+            host= 'localhost',
+            user= 'root',
+            password = 1234,
+            database = 'Intelligence_db'
+            )
+        
+    def get_conncation(self):
+        if self.conn is None or not self.conn.is_connected():
+            self.connect()
+        return self.conn
+    
+    def create_database(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS Intelligence_db")
+            cursor.execute("USE Intelligence_db")
+            self.conn.commit()
+
+
+    def create_tables(self):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS agents(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(30) NOT NULL,
+            specialty VARCHAR(50) NOT NULL,
+            is_active BOOLEAN DEAFULT TRUE,
+            completed_missions INT DEAFULT 0,
+            failed_missions INT DEAFULT 0,
+            agent_rank ENUM('Junior', 'Senior', 'Commander') NOT NULL,
+            )
+            '''
+            )
+        self.conn.commit()
+
+        cursor.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS missions(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(50) NOT NULL,
+            description TEXT NOT NULL,
+            location VARCHAR(100) NOT NULL,
+            difficulty INT NOT NULL,
+            importance INT NOT NULL,
+            status VARCHAR(30) DEAFULT NEW,
+            risk_level VARCHAR(30) NOT NULL,
+            assigned_agent_id INT DEAFULT NULL
+            )
+            '''
+        )
+        self.conn.commit()
+        
+        
+    def close_db(self):
+        self.conn.close()
+
+DB = DB_connection()
+
+
